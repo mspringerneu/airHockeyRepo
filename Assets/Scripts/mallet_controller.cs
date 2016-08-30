523788009896c2d32bdaf6e2e92e08b9495f151d
Matt Springer <springer.m@husky.neu.edu>
2016-08-29 21:05:50 -0400

    Beginning of PlayerController, adding GameController and Colliders

--- Assets/Scripts/mallet_controller.cs
+++ Assets/Scripts/mallet_controller.cs
@@ -2,6 +2,12 @@
 using System.Collections;
 
 public class mallet_controller : MonoBehaviour {
+	private Vector3 initPos = new Vector3 (2.25f, 0.1f, 6f);
+	private LayerMask tableLayer;
+	private GameObject controller;
+	private Vector3 targetPoint;
+	public float inputPadding = 0.0f;
+	public float inputSpeed = 2.0f;
 
 	// mallet diameter 4 1/2"
 
@@ -19,11 +25,32 @@ public class mallet_controller : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		tableLayer = 1 << LayerMask.NameToLayer ("Table");
+		transform.position = initPos;
+		controller = GameObject.FindGameObjectWithTag ("GameController");
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+		Ray mouseRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+
+		if (Physics.Raycast(mouseRay, out hit, tableLayer)) {
+			targetPoint = hit.point;
+		}
 	}
+
+	void Update() {
+		if (Vector3.Distance(transform.position, targetPoint) > inputPadding) {
+			Vector3 pos  = Vector3.Lerp(transform.position, targetPoint, Time.deltaTime * inputSpeed);
+			transform.position = new Vector3(pos.x, transform.position.y, pos.z);
+		}
+	}
+	/*
+	void newPoint(bool whoseServe) {
+		if (controller.isPlayerServe) {
+			
+		}
+	}
+	*/
 }
--- Assets/Scripts/puck_script.cs
+++ Assets/Scripts/puck_script.cs
@@ -2,7 +2,9 @@
 using System.Collections;
 
 public class puck_script : MonoBehaviour {
-
+	private Vector3 centerPos = new Vector3(1.625f, 0.5f, 46.5f);
+	private Vector3 playerInitPos = new Vector3(1.625f, 0.5f, 11f);
+	private Vector3 opponentInitPos = new Vector3(1.625f, 0.5f, 82f);
 	// puck diameter 3.25"
 
 	// Use this for initialization
